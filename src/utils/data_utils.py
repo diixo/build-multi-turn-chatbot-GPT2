db4a5bffa931a57728941ddde86d237c40c169c9
diixo <anatolii.podoba@gmail.com>
2026-03-17 06:16:22 +0200

    data_utils.py: DialogLoader refactoring for labeling correct pairs: stage=1

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -1,82 +1,100 @@
-import random
-import numpy as np
+
 from copy import deepcopy
+from typing import List, Dict, Any, Optional, Tuple
 
 import torch
 from torch.utils.data import Dataset
 
-from utils import colorstr
-
-
-
-def seed_worker(worker_id):  # noqa
-    """Set dataloader worker seed https://pytorch.org/docs/stable/notes/randomness.html#dataloader."""
-    worker_seed = torch.initial_seed() % 2 ** 32
-    np.random.seed(worker_seed)
-    random.seed(worker_seed)
 
 
 class DialogLoader(Dataset):
 
-    def __init__(self, data, tokenizer, config):
-        self.data = data
+    IGNORE_INDEX = -100
+
+    def __init__(self, data: List[Tuple[str, ...]], tokenizer, config):
         self.tokenizer = tokenizer
         self.max_len = config.max_len
+
+        if self.tokenizer.eos_token_id is None:
+            raise ValueError("tokenizer.eos_token_id must not be None")
+
+        if self.tokenizer.pad_token_id is None:
+            raise ValueError(
+                "tokenizer.pad_token_id must not be None. "
+                "For GPT-2 usually set tokenizer.pad_token = tokenizer.eos_token"
+            )
+
+        self.turn_sep_id = self.tokenizer.eos_token_id
+
+        self.data = []
+        for dialog in data:
+            self.data.extend([dialog, dialog])
+
         self.length = len(self.data)
 
 
-    def make_data(self, multi_turn_sentences):
-        all_turns_tokens = [self.tokenizer.cls_token_id]
-        label_tokens = [self.tokenizer.pad_token_id]
+    @staticmethod
+    def _pad(x: list, length: int, pad_value: int) -> list:
+        if len(x) >= length:
+            return x[:length]
+        return x + [pad_value] * (length - len(x))
+
+
+    def make_data(self, multi_turn_sentences: Tuple[str, ...], predict_parity: int):
+        input_ids = []
+        labels = []
 
         for i, sentence in enumerate(multi_turn_sentences):
-            # source tokens
-            sentence_token = self.tokenizer.encode(sentence) + [self.tokenizer.sep_token_id]
-            all_turns_tokens.extend(sentence_token)
+            sentence_tokens = self.tokenizer.encode(sentence)
+            sentence_tokens.append(self.turn_sep_id)
 
-            if i == 0:
-                first_sentence = self.padding(deepcopy(all_turns_tokens)[:self.max_len], self.max_len, self.tokenizer.pad_token_id)
-                first_sentence_l = len(all_turns_tokens)
-
-            # label tokens
-            if i % 2  == 1:
-                label_tokens.extend(sentence_token)
-            else:
-                label_tokens.extend([self.tokenizer.pad_token_id] * len(sentence_token))
-            
-            if len(all_turns_tokens) >= self.max_len:
+            remaining = self.max_len - len(input_ids)
+            if remaining <= 0:
                 break
-        
-        # Adding EOS token at the end when the length of tokens cannot be reached to the max length
-        # even though all turns are appended
-        if len(all_turns_tokens) < self.max_len:
-            # source tokens
-            all_turns_tokens += [self.tokenizer.eos_token_id]
-            all_turns_tokens = self.padding(all_turns_tokens, self.max_len, self.tokenizer.pad_token_id)
-            
-            # label tokens
-            label_tokens += [self.tokenizer.eos_token_id]
-            label_tokens = self.padding(label_tokens, self.max_len, self.tokenizer.pad_token_id)
-        else:
-            all_turns_tokens = all_turns_tokens[:self.max_len]
-            label_tokens = label_tokens[:self.max_len]
 
-        assert len(all_turns_tokens) == len(label_tokens) == self.max_len, \
-            colorstr(f'The token length must be equal to the max length of the config.yaml. Expected {self.max_len}, but got {len(all_turns_tokens)}')
+            if len(sentence_tokens) > remaining:
+                sentence_tokens = sentence_tokens[:remaining]
 
-        return all_turns_tokens, label_tokens, first_sentence, first_sentence_l
-    
+            input_ids.extend(sentence_tokens)
 
-    @staticmethod
-    def padding(x:list, length, pad_token_id):
-        x += [pad_token_id] * (length - len(x))
-        return x
+            if i % 2 == predict_parity:
+                labels.extend(sentence_tokens)
+            else:
+                labels.extend([self.IGNORE_INDEX] * len(sentence_tokens))
+
+            if len(input_ids) >= self.max_len:
+                break
+
+        real_len = len(input_ids)
+
+        input_ids = self._pad(input_ids, self.max_len, self.tokenizer.pad_token_id)
+        labels = self._pad(labels, self.max_len, self.IGNORE_INDEX)
+        attention_mask = [1] * real_len + [0] * (self.max_len - real_len)
+
+        assert len(input_ids) == self.max_len
+        assert len(labels) == self.max_len
+        assert len(attention_mask) == self.max_len
+
+        return input_ids, labels, attention_mask
 
 
-    def __getitem__(self, idx):
-        x, y, fs, fsl = self.make_data(self.data[idx])
-        return torch.tensor(x, dtype=torch.long), torch.tensor(y, dtype=torch.long), \
-                torch.tensor(fs, dtype=torch.long), torch.tensor(fsl, dtype=torch.long)
+    def __getitem__(self, idx: int):
+        multi_turn_sentences = self.data[idx]
+        predict_parity = idx % 2
+
+        for i, sent in enumerate(multi_turn_sentences):
+            print(f"Tokens[{i}]: {self.tokenizer.tokenize(sent)}")
+
+        input_ids, labels, attention_mask = self.make_data(
+            multi_turn_sentences=multi_turn_sentences,
+            predict_parity=predict_parity
+        )
+
+        return {
+            "input_ids": torch.tensor(input_ids, dtype=torch.long),
+            "labels": torch.tensor(labels, dtype=torch.long),
+            "attention_mask": torch.tensor(attention_mask, dtype=torch.long),
+        }
 
 
     def __len__(self):
